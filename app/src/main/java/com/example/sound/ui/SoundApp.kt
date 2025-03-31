d844b25897fcc595b1f44470378d99614f4e9bcf
2uan2 <2uan20004@gmail.com>
2025-03-31 10:25:03 +0700

    added barebone navigation between screens

--- app/src/main/java/com/example/sound/ui/SoundApp.kt
+++ app/src/main/java/com/example/sound/ui/SoundApp.kt
@@ -1,10 +1,50 @@
 package com.example.sound.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Screen {
+    MAIN,
+    ALBUM,
+    PLAYER,
+
+}
 
 @Composable
 fun SoundApp(
 
 ) {
+    val navController: NavHostController = rememberNavController()
 
+    Scaffold(
+
+    ) { innerPadding ->
+        NavHost(
+            navController,
+            startDestination = Screen.MAIN.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = Screen.MAIN.name) {
+                MainScreen(
+
+                )
+            }
+            composable(route = Screen.ALBUM.name) {
+                AlbumScreen(
+
+                )
+            }
+            composable(route = Screen.PLAYER.name) {
+                PlayerScreen(
+
+                )
+            }
+        }
+    }
 }
